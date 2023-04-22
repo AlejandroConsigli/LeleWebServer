ff6c2016dee3886322312ea624ba5fb94518d9b7
Alejandro <consiglialejandro@gmail.com>
2023-04-22 16:42:10 +0200

    Pre commit fix

--- build/index.js
+++ build/index.js
@@ -18,6 +18,6 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.listen(PORT);
 app.get("/api", (_req, res) => {
-    res.status(200).json({ message: "Lele Web Greetings Server Ja Noww" });
+    res.status(200).json({ message: "Lele Web Greetings Server" });
 });
 app.use("/api/greetings", greetings_1.default);
